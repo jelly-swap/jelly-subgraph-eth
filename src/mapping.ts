e1b1aaa276837891b5c7a2a23251428b35fd9dab
Andon Mitev <mitevandon94@gmail.com>
2020-04-01 10:58:35 +0300

    Rename Entities cuz were matching with name of the events

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,23 +5,19 @@ import {
   Refund,
   Withdraw
 } from "../generated/Contract/Contract";
-import { Jelly } from "../generated/schema";
+import {
+  NewContractEntity,
+  WithdrawEntity,
+  RefundEntity
+} from "../generated/schema";
 
 export function handleNewContract(event: NewContract): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = Jelly.load(event.transaction.from.toHex());
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
+  let entity = NewContractEntity.load(event.transaction.from.toHex());
+
   if (entity == null) {
-    entity = new Jelly(event.transaction.from.toHex());
-    // Entity fields can be set using simple assignments
-    //   entity.count = BigInt.fromI32(0)
-    // }
+    entity = new NewContractEntity(event.transaction.from.toHex());
   }
-  // BigInt and BigDecimal math are supported
-  // entity.count = entity.count + BigInt.fromI32(1)
-  // Entity fields can be set based on event parameters
+
   entity.inputAmount = event.params.inputAmount;
   entity.outputAmount = event.params.outputAmount;
   entity.expiration = event.params.expiration;
@@ -32,32 +28,11 @@ export function handleNewContract(event: NewContract): void {
   entity.outputNetwork = event.params.outputNetwork;
   entity.outputAddress = event.params.outputAddress;
 
-  // Entities can be written to the store with `.save()`
   entity.save();
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.ACTIVE(...)
-  // - contract.EXPIRED(...)
-  // - contract.INVALID(...)
-  // - contract.REFUNDED(...)
-  // - contract.WITHDRAWN(...)
-  // - contract.contracts(...)
-  // - contract.getSingleStatus(...)
-  // - contract.getStatus(...)
 }
 
-export function handleRefund(event: Refund): void {}
+export function handleRefund(event: Refund): void {
+  // let refund = Refundd.load();
+}
 
 export function handleWithdraw(event: Withdraw): void {}
